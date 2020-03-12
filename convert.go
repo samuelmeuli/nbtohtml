b7c1a48589394fbea5158be983f041f1612b9a74
Samuel Meuli <me@samuelmeuli.com>
2020-03-13 00:20:26 +0700

    Handle different cell formats

--- convert.go
+++ convert.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/alecthomas/chroma"
+	"github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
+	"github.com/russross/blackfriday/v2"
+	"io"
 	"io/ioutil"
+	"strings"
 )
+
+// highlightCode uses Chroma to convert the provided source code string to HTML with tags and
+// classes for syntax highlighting.
+func highlightCode(writer io.Writer, source string, lexer string) error {
+	l := lexers.Get(lexer)
+	if l == nil {
+		l = lexers.Analyse(source)
+	}
+	if l == nil {
+		l = lexers.Fallback
+	}
+	l = chroma.Coalesce(l)
+
+	// Configure Chroma to use classes instead of inline styles
+	formatter := html.New(html.WithClasses(true))
+
+	iterator, err := l.Tokenise(nil, source)
+	if err != nil {
+		return err
+	}
+	return formatter.Format(writer, styles.GitHub, iterator)
+}
 
 // ConvertFileToHTML reads the file at the provided path and converts its content (the Jupyter
 // Notebook JSON) to HTML.
@@ -25,11 +55,38 @@ func ConvertStringToHTML(notebookString string) (string, error) {
 		return "", err
 	}
 
+	// Get format extension of Jupyter Kernel language (e.g. "py")
+	fileExtension := notebook.Metadata.LanguageInfo.FileExtension[1:]
+
 	// Build HTML string from converted cells
-	html := ""
+	htmlString := "<div class=\"jupyter-notebook\">"
 	for _, cell := range notebook.Cells {
-		html += fmt.Sprintf("<p>%s/p>", cell.CellType) // TODO: Replace with conversion logic
+		switch cell.CellType {
+		case "markdown":
+			// Markdown cell: Convert to HTML with Blackfriday
+			md := strings.Join(cell.Source, "")
+			mdHTML := blackfriday.Run([]byte(md))
+			htmlString += fmt.Sprintf("<div class=\"cell markdown-cell\">%s</div>", string(mdHTML))
+		case "code":
+			// Code cell: Convert to HTML for syntax highlighting with Chroma
+			codeString := strings.Join(cell.Source, "")
+			codeBuffer := new(bytes.Buffer)
+			err := highlightCode(codeBuffer, codeString, fileExtension)
+			if err == nil {
+				htmlString += fmt.Sprintf("<div class=\"cell code-cell\">%s</div>", codeBuffer.String())
+			} else {
+				fmt.Printf("skipping cell (syntax highlighting error: %d)", err)
+			}
+		case "raw":
+			// Raw cell: Create simple HTML string
+			htmlString += fmt.Sprintf(
+				"<div class=\"cell raw-cell\"><pre>%s</pre></div>",
+				strings.Join(cell.Source, ""),
+			)
+		default:
+			fmt.Printf("skipping cell (unrecognized cell type \"%s\")", cell.CellType)
+		}
 	}
 
-	return html, nil
+	return htmlString + "</div>", nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/samuelmeuli/notebook-to-html
 go 1.14
 
 require (
+	github.com/alecthomas/chroma v0.7.1
 	github.com/alecthomas/kong v0.2.4
+	github.com/dlclark/regexp2 v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
 )
