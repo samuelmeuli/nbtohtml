168c8669107cda8f4ddeeec5e217d1fb5de462a9
Samuel Meuli <me@samuelmeuli.com>
2020-03-13 16:36:11 +0700

    Remove unused CLI flags

--- cmd/nbtohtml/main.go
+++ cmd/nbtohtml/main.go
@@ -7,20 +7,11 @@ import (
 	"os"
 )
 
-type context struct {
-	Debug bool
-}
-
 type convertCmd struct {
-	IncludeCodeCSS     bool   `help:"Whether styles for syntax highlighting in code should be included in the HTML" default:"true"`
-	IncludeNotebookCSS bool   `help:"Whether styles for the Jupyter Notebook (e.g. cells and Markdown) should be included in the HTML" default:"true"`
-	CodeLightStyle     string `help:"Chroma style to use in light mode" default:"github" enum:"abap,algol,algol_nu,arduino,autumn,borland,bw,colorful,dracula,emacs,friendly,fruity,github,igor,lovelace,manni,monokai,monokailight,murphy,native,paraiso-dark,paraiso-light,pastie,perldoc,pygments,rainbow_dash,rrt,solarized-dark,solarized-dark256,solarized-light,swapoff,tango,trac,vim,vs,xcode"`
-	CodeDarkStyle      string `help:"Chroma style to use in dark mode" default:"dracula" enum:"abap,algol,algol_nu,arduino,autumn,borland,bw,colorful,dracula,emacs,friendly,fruity,github,igor,lovelace,manni,monokai,monokailight,murphy,native,paraiso-dark,paraiso-light,pastie,perldoc,pygments,rainbow_dash,rrt,solarized-dark,solarized-dark256,solarized-light,swapoff,tango,trac,vim,vs,xcode"`
-
 	Path string `arg name:"path" help:"Jupyter Notebook file to convert" type:"path"`
 }
 
-func (r *convertCmd) Run(ctx *context) error {
+func (r *convertCmd) Run() error {
 	var notebookPath = r.Path
 
 	// Make sure notebook file exists
@@ -49,7 +40,6 @@ var cli struct {
 
 func main() {
 	ctx := kong.Parse(&cli)
-	// Call the Run() method of the selected parsed command
-	err := ctx.Run(&context{Debug: cli.Debug})
+	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
 }
